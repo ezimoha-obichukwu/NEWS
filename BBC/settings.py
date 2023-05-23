03c2974b44d0b24adf84f2d5ddb366e11291a4ac
OBI <77929524+ezimoha-obichukwu@users.noreply.github.com>
2023-05-23 00:31:37 -1200

    'third'

--- BBC/settings.py
+++ BBC/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$(g5#lbtevlv0qr-@5+w6so0p2%57-p%_+#b7eh9h32r%ny(od'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,13 +76,23 @@ WSGI_APPLICATION = 'BBC.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {     
+'default': {
+ 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+ 'NAME': 'News',
+ 'USER': 'postgres',
+ 'PASSWORD': config('DATABASE_PASSWORD'),
+ 'HOST': 'localhost',
+ 'PORT': '5433'  
+} 
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- News/views.py
+++ News/views.py
@@ -18,5 +18,24 @@ class news_home_page(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
+class news_detail_page(APIView):
+    def get(self, request, slug, format=None):
+        single_news = News.objects.get(slug=slug)
+        serialized = Newsserializer(single_news)
+        return Response(serialized.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, slug, format=None):
+        single_comment = News.objects.get(slug=slug)
+        serializer = Newsserializer(single_comment, data=request.data, partial=True)
+        serializer.is_valid()
+        serializer.save()
+        return Response({"message": "News update Successfully",
+                          "data": serializer.data}, status=status.HTTP_202_ACCEPTED)
+    
+    
+    def delete(self, request, slug, format=None):
+        single_comment = News.objects.get(slug=slug)
+        single_comment.delete()
+        return Response("News has been deleted", status=status.HTTP_204_NO_CONTENT)
 
 
